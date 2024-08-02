32567cef4105e5014941089f8cbe267f1044476f
ryeonng <zrkfudz29@naver.com>
2024-08-02 15:21:10 +0900

    first commit

---
+++ src/main/java/com/tenco/bank/ClassBank2Application.java
@@ -0,0 +1,13 @@
+package com.tenco.bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassBank2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassBank2Application.class, args);
+	}
+
+}
